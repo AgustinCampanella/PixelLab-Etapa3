3f74cc1546eee6aff0a3065c1fc366021fa0d779
AgustinCampanella <campanellagustin@hotmail.com>
2023-11-20 21:04:51 -0300

    Orbital

--- Juego/Niveles/Nivel.gd
+++ Juego/Niveles/Nivel.gd
@@ -34,6 +34,7 @@ func conectar_seniales() -> void:
 	Eventos.connect("meteorito_destruido", self, "_on_meteorito_destruido")
 	Eventos.connect("nave_en_sector_peligro", self, "_on_nave_en_sector_peligro")
 	Eventos.connect("base_destruida", self, "_on_base_destruida")
+	Eventos.connect("spawn_orbital", self, "_on_spawn_orbital")
 
 func crear_contenedores() -> void:
 	contenedor_proyectiles = Node.new()
@@ -113,6 +114,21 @@ func crear_sector_enemigos(num_enemigos: int) -> void:
 		contenedor_enemigos.add_child(new_interceptor)
 
 
+func crear_explosion(
+	posicion: Vector2, 
+	numero: int = 1, 
+	intervalo: float = 0.0, 
+	rangos_aleatorios: Vector2 = Vector2(0.0, 0.0)) -> void:
+		for i in range(numero):
+			var new_explosion:Node2D = explosion.instance()
+			new_explosion.global_position = posicion + crear_posicion_aleatoria(
+				rangos_aleatorios.x,
+				rangos_aleatorios.y
+			)
+			add_child(new_explosion)
+			yield(get_tree().create_timer(intervalo), "timeout")
+
+
 ## Conexion señales externas
 func _on_disparo(proyectil:Area2D) -> void:
 	contenedor_proyectiles.add_child(proyectil)
@@ -154,22 +170,10 @@ func _on_base_destruida(pos_partes: Array) -> void:
 		crear_explosion(posicion)
 		yield(get_tree().create_timer(0.5), "timeout")
 
-func crear_explosion(
-	posicion: Vector2, 
-	numero: int = 1, 
-	intervalo: float = 0.0, 
-	rangos_aleatorios: Vector2 = Vector2(0.0, 0.0)) -> void:
-		for i in range(numero):
-			var new_explosion:Node2D = explosion.instance()
-			new_explosion.global_position = posicion + crear_posicion_aleatoria(
-				rangos_aleatorios.x,
-				rangos_aleatorios.y
-			)
-			add_child(new_explosion)
-			yield(get_tree().create_timer(intervalo), "timeout")
-
-
 
 func _on_Tween_tween_completed(object: Object, _key: NodePath) -> void:
 	if object.name == "CameraPlayer":
 		object.global_position = $Player.global_position
+
+func _on_spawn_orbital(enemigo: EnemigoOrbital) -> void:
+	contenedor_enemigos.add_child(enemigo)
